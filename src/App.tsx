92d4b9dbceaeb855da957d981aa7705cd92990e5
leeedev <leeesangheee@gmail.com>
2021-06-05 01:19:33 +0900

    Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import AppRouter from "router/AppRouter";
 
 function App() {
-  return <div className="App">React App</div>;
+  return <AppRouter />;
 }
 
 export default App;
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "screens/Home";
+import Login from "screens/Login";
+import NotFound from "screens/NotFound";
+
+const AppRouter = () => {
+  const isLoggedIn = false;
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
